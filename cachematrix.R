1c1d6399eb5c7e3cf9d9ba821e18a74710eadbe5
Aritz <aritzsanc@yahoo.es>
2015-09-24 13:01:15 +0200

    add comments and change the order of the operations in cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,19 @@
 ## This pair of functions caches the inverse of a matrix.
 
 ## This function creates a special "matrix" object that can cache its inverse
-
 makeCacheMatrix <- function(x = matrix()) {
+  #First it creates a NULL object that will contain the inverse once it is calcullated
   inverse_x <- NULL
+  #when the matrix changes the inverse is not valid so it is assigned to NULL
   set <- function(y) {
     x <<- y
     inverse_x <<- NULL
   }
+  #get returns the matrix x
   get <- function() x
+  #setinverse stores a value in inverse_x and caches it (<<-)
   setinverse <- function(solve) inverse_x <<- solve
+  #getinverse returns the value of the inverse matric of x
   getinverse <- function() inverse_x
   list(set = set, get = get,
        setinverse = setinverse,
@@ -22,15 +26,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then the cachesolve should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-  inverse_x <- x$getinverse()
-  if(!is.null(inverse_x)) {
-    message("getting cached data")
-    return(inverse_x)
+  #read the value of the inverse matrix of x
+  if(is.null(x$getinverse())) {
+    #if the value of inverse_x is NULL it is neccesary to calcullate the value
+    x$setinverse(solve(x$get()))
+    message("Matrix changed.")
+    message("Inverse re-computed")
   }
-  data <- x$get()
-  inverse_x <- solve(data)
-  x$setinverse(inverse_x)
-  message("Matrix changed.")
-  message("Inverse re-computed")
-  inverse_x
+  else{
+    #if the readed value is not NULL the cached inverse matrix is valid, so its value is returned and the function ends
+    message("getting cached data")
+  }
+  x$getinverse()
 }
